9f821b24906d5728f05d319150bec4bd7b838ac0
Bunyarit Puangthamawathanakun <aofpcc@Bunyarits-MacBook-Pro.local>
2020-05-01 20:43:11 +0700

    [ADD] add circle

--- App.jsx
+++ App.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { StyleSheet, Dimensions, StatusBar } from "react-native";
 import Matter from "matter-js";
 import Box from "./src/Box";
+import Circle from "./src/Circle";
 import { GameEngine } from "react-native-game-engine";
 
 const { width, height } = Dimensions.get("screen");
 const boxSize = Math.trunc(Math.max(width, height) * 0.075);
+const radius = Math.trunc(Math.max(width, height) * 0.075) * 0.5;
 const engine = Matter.Engine.create({ enableSleeping: false });
 const world = engine.world;
-const initialBox = Matter.Bodies.rectangle(
+// const initialBox = Matter.Bodies.rectangle(
+//   width / 2,
+//   height / 2,
+//   boxSize,
+//   boxSize
+// );
+const initialCircle = Matter.Bodies.circle(
   width / 2,
   height / 2,
-  boxSize,
-  boxSize
+  radius
 );
 const floor = Matter.Bodies.rectangle(
   width / 2,
@@ -21,7 +28,7 @@ const floor = Matter.Bodies.rectangle(
   boxSize,
   { isStatic: true }
 );
-Matter.World.add(world, [initialBox, floor]);
+Matter.World.add(world, [initialCircle, floor]);
 
 const Physics = (entities, { time }) => {
   let engine = entities["physics"].engine;
@@ -31,17 +38,45 @@ const Physics = (entities, { time }) => {
 
 let boxIds = 0;
 
-const CreateBox = (entities, { touches, screen }) => {
+// const CreateBox = (entities, { touches, screen }) => {
+//   let world = entities["physics"].world;
+//   let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
+//   touches
+//     .filter((t) => t.type === "press")
+//     .forEach((t) => {
+//       let body = Matter.Bodies.rectangle(
+//         t.event.pageX,
+//         t.event.pageY,
+//         boxSize,
+//         boxSize,
+//         {
+//           frictionAir: 0.021,
+//           restitution: 1.5,
+//         }
+//       );
+
+//       Matter.World.add(world, [body]);
+
+//       entities[++boxIds] = {
+//         body: body,
+//         size: [boxSize, boxSize],
+//         color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+//         renderer: Box,
+//       };
+//     });
+//   return entities;
+// };
+
+const CreateCircle = (entities, { touches, screen }) => {
   let world = entities["physics"].world;
-  let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
+  let radius = Math.trunc(Math.max(screen.width, screen.height) * 0.075) * 0.5;
   touches
     .filter((t) => t.type === "press")
     .forEach((t) => {
-      let body = Matter.Bodies.rectangle(
+      let body = Matter.Bodies.circle(
         t.event.pageX,
         t.event.pageY,
-        boxSize,
-        boxSize,
+        radius,
         {
           frictionAir: 0.021,
           restitution: 1.5,
@@ -52,9 +87,9 @@ const CreateBox = (entities, { touches, screen }) => {
 
       entities[++boxIds] = {
         body: body,
-        size: [boxSize, boxSize],
+        radius: radius,
         color: boxIds % 2 == 0 ? "pink" : "#B8E986",
-        renderer: Box,
+        renderer: Circle,
       };
     });
   return entities;
@@ -65,17 +100,17 @@ export default class App extends React.Component {
     return (
       <GameEngine
         style={styles.container}
-        systems={[Physics, CreateBox]}
+        systems={[Physics, CreateCircle]}
         entities={{
           physics: {
             engine: engine,
             world: world,
           },
-          initialBox: {
-            body: initialBox,
-            size: [boxSize, boxSize],
+          initialCircle: {
+            body: initialCircle,
+            radius: radius,
             color: "red",
-            renderer: Box,
+            renderer: Circle,
           },
           floor: {
             body: floor,
---
+++ src/Circle.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import { number, object, string } from 'prop-types';
+
+export default class Circle extends Component {
+  render() {
+    const radius = this.props.radius;
+    const x = this.props.body.position.x - radius;
+    const y = this.props.body.position.y - radius;
+    
+    return (
+      <View
+        style={{
+            position: "absolute",
+            left: x,
+            top: y,
+            width: radius * 2,
+            height: radius * 2,
+            borderRadius: '100%',
+            backgroundColor: this.props.color || "pink"
+          }}/>
+    );
+  }
+}
+
+Circle.propTypes = {
+    radius: number,
+    body: object, 
+    color: string
+}
