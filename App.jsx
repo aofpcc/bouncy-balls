fed5f92218614a97c6c0d5ab5e62275b3aef7d9b
Bunyarit Puangthamawathanakun <aofpcc@Bunyarits-MacBook-Pro.local>
2020-05-01 17:36:26 +0700

    [ADD] something

--- App.jsx
+++ App.jsx
@@ -4,63 +4,72 @@ import Matter from "matter-js";
 import Box from "./src/Box";
 import { GameEngine } from "react-native-game-engine";
 
+const { width, height } = Dimensions.get("screen");
+const boxSize = Math.trunc(Math.max(width, height) * 0.075);
+const engine = Matter.Engine.create({ enableSleeping: false });
+const world = engine.world;
+const initialBox = Matter.Bodies.rectangle(
+  width / 2,
+  height / 2,
+  boxSize,
+  boxSize
+);
+const floor = Matter.Bodies.rectangle(
+  width / 2,
+  height - boxSize,
+  width,
+  boxSize,
+  { isStatic: true }
+);
+Matter.World.add(world, [initialBox, floor]);
+
+const Physics = (entities, { time }) => {
+  let engine = entities["physics"].engine;
+  Matter.Engine.update(engine, time.delta);
+  return entities;
+};
+
+let boxIds = 0;
+
+const CreateBox = (entities, { touches, screen }) => {
+  let world = entities["physics"].world;
+  let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
+  touches
+    .filter((t) => t.type === "press")
+    .forEach((t) => {
+      let body = Matter.Bodies.rectangle(
+        t.event.pageX,
+        t.event.pageY,
+        boxSize,
+        boxSize,
+        {
+          frictionAir: 0.021,
+          restitution: 1.5,
+        }
+      );
+
+      Matter.World.add(world, [body]);
+
+      entities[++boxIds] = {
+        body: body,
+        size: [boxSize, boxSize],
+        color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+        renderer: Box,
+      };
+    });
+  return entities;
+};
+
 export default class App extends React.Component {
   render() {
-    const { width, height } = Dimensions.get("screen");
-    const boxSize = Math.trunc(Math.max(width, height) * 0.075);
-    const initialBox = Matter.Bodies.rectangle(
-      width / 2,
-      height / 2,
-      boxSize,
-      boxSize
-    );
-
-    const floor = Matter.Bodies.rectangle(
-      width / 2,
-      height - boxSize,
-      width,
-      boxSize,
-      { isStatic: true }
-    );
-
-    const engine = Matter.Engine.create({ enableSleeping: false });
-    const world = engine.world;
-    Matter.World.add(world, [initialBox, floor]);
-
-    const Physics = (entities, { time }) => {
-      let engine = entities["physics"].engine;
-      Matter.Engine.update(engine, time.delta);
-      return entities;
-    };
-
-    let boxIds = 0;
-    const CreateBox = (entities, { touches, screen }) => {
-        let world = entities["physics"].world;
-        let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
-        touches.filter(t => t.type === "press").forEach(t => {
-                let body = Matter.Bodies.rectangle(
-                          t.event.pageX, t.event.pageY, 
-                          boxSize, boxSize,
-                          { frictionAir: 0.021, restitution: 1.0  });
-                Matter.World.add(world, [body]);
-                    entities[++boxIds] = {
-                        body: body,
-                        size: [boxSize, boxSize],
-                        color: boxIds % 2 == 0 ? "pink" : "#B8E986",
-                        renderer: Box
-                    };
-                });
-        return entities;
-    };
-
     return (
       <GameEngine
         style={styles.container}
-        systems={[Physics,CreateBox]}
+        systems={[Physics, CreateBox]}
         entities={{
-          physics: { 
-            engine: engine, 
-            world: world 
+          physics: {
+            engine: engine,
+            world: world,
           },
           initialBox: {
             body: initialBox,
@@ -74,7 +83,8 @@ export default class App extends React.Component {
             color: "green",
             renderer: Box,
           },
-        }}>
+        }}
+      >
         <StatusBar hidden={true} />
       </GameEngine>
     );
