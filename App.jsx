0f0e015174c995f1e7eaefcf0574f8c6a240d323
Bunyarit Puangthamawathanakun <aofpcc@Bunyarits-MacBook-Pro.local>
2020-05-01 17:30:10 +0700

    [ADD] new system: touch to create a box

--- App.jsx
+++ App.jsx
@@ -33,10 +33,30 @@ export default class App extends React.Component {
       return entities;
     };
 
+    let boxIds = 0;
+    const CreateBox = (entities, { touches, screen }) => {
+        let world = entities["physics"].world;
+        let boxSize = Math.trunc(Math.max(screen.width, screen.height) * 0.075);
+        touches.filter(t => t.type === "press").forEach(t => {
+                let body = Matter.Bodies.rectangle(
+                          t.event.pageX, t.event.pageY, 
+                          boxSize, boxSize,
+                          { frictionAir: 0.021 });
+                Matter.World.add(world, [body]);
+                    entities[++boxIds] = {
+                        body: body,
+                        size: [boxSize, boxSize],
+                        color: boxIds % 2 == 0 ? "pink" : "#B8E986",
+                        renderer: Box
+                    };
+                });
+        return entities;
+    };
+
     return (
       <GameEngine
         style={styles.container}
-        systems={[Physics]}
+        systems={[Physics,CreateBox]}
         entities={{
           physics: { 
             engine: engine, 
